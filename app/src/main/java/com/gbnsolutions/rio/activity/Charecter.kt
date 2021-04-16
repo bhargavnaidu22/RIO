36c6ef2825c8262ccf101f194b227f06303ede6b
bharg <bhargavnaidu.gedela2002@gmail.com>
2021-04-16 20:07:24 +0530

    RIO

--- app/src/main/java/com/gbnsolutions/rio/activity/Charecter.kt
+++ app/src/main/java/com/gbnsolutions/rio/activity/Charecter.kt
@@ -1,23 +1,14 @@
 package com.gbnsolutions.rio.activity
 
-import android.accounts.AccountManager.get
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.MenuItem
-import android.widget.FrameLayout
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.coordinatorlayout.widget.CoordinatorLayout
-import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.viewpager.widget.ViewPager
 import com.gbnsolutions.rio.R
 import com.gbnsolutions.rio.adapter.FragmentAdapter
 import com.gbnsolutions.rio.fragment.HomeFragment
 import com.gbnsolutions.rio.fragment.ProfileFragment
 import com.gbnsolutions.rio.fragment.StatusFragment
-import com.google.android.material.navigation.NavigationView
 import com.google.android.material.tabs.TabLayout
 
 
@@ -35,10 +26,12 @@ class Charecter : AppCompatActivity() {
         setUpToolbar()
         setupTabs()
     }
+
     fun setUpToolbar() {
         setSupportActionBar(toolbar)
         supportActionBar?.title = "RIO"
     }
+
     private fun setupTabs() {
         val adapter = FragmentAdapter(supportFragmentManager)
         adapter.addFragment(HomeFragment(), "Home")
